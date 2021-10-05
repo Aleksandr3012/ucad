bd34d996c5a359b624454146bda7b67a0091762f
Aleksandr3012 <flaming541@gmail.com>
2021-10-05 13:28:21 +0300

    test

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -1,52 +1,40 @@
 "use strict";
-
-const JSCCommon = {
-	heightwindow() {
-		// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-		let vh = window.innerHeight * 0.01;
-		// Then we set the value in the --vh custom property to the root of the document
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-		// We listen to the resize event
-		window.addEventListener('resize', () => {
-			// We execute the same script as before
-			let vh = window.innerHeight * 0.01;
-			document.documentElement.style.setProperty('--vh', `${vh}px`);
-		}, { passive: true });
-	},
-
-};
-
 // const $ = jQuery;
-
 function eventHandler() {
 
-	JSCCommon.heightwindow();
+	function getFromAPI(url, callback) {
+		var obj;
 
+		fetch (url)
+			.then(res => res.json())
+			.then(data => obj = data)
+			.then(() => callback(obj));
+	}
 
-	// function setFixedNav() {
-	// 	let topNav = document.querySelector('.top-nav  ');
-	// 	if (!topNav) return;
-	// 	window.scrollY > 0
-	// 		? topNav.classList.add('fixed')
-	// 		: topNav.classList.remove('fixed');
-	// }
+	const createItem = data => {
+		const card = `
+								<div class="headerBlock__imgWrap">
+									<img src="${data?.image?.url}" alt="${data?.name}" width="${data?.image?.width}}" height="${data?.image?.height}" loading="lazy">
+								</div>
+								<div class="headerBlock__txt">${data?.life_span}</div>
+								<div class="headerBlock__title">${data?.bred_for}</div>
+								<p>${data?.name}</p>`
 
-	// function whenResize() {
-	// 	setFixedNav();
-	// }
+		const liParrent = document.createElement('div');
+		liParrent.className = 'swiper-slide',
+		liParrent.innerHTML = card;
+		return liParrent;
+	};
 
-	// window.addEventListener('scroll', () => {
-	// 	setFixedNav();
+	function drawList (arrOfObjs) {
+		arrOfObjs.map(item => {
+			return document.querySelector('#testList').append(createItem(item));
+		});
+	}
 
-	// }, { passive: true })
-	// window.addEventListener('resize', () => {
-	// 	whenResize();
-	// }, { passive: true });
+	getFromAPI('https://api.thedogapi.com/v1/breeds?limit=10&page=0', drawList);
 
-	// whenResize();
-
-	const swiper = new Swiper('.headerBlock .mySwiper', {
+	const swiper = new Swiper('.headerBlock__slider--js', {
 		slidesPerView: 1,
 		spaceBetween: 20,
 		breakpoints: {
@@ -57,32 +45,16 @@ function eventHandler() {
 				slidesPerView: 3,
 			}
 		},
-
 		navigation: {
 			nextEl: '.headerBlock .swiper-button-next',
 			prevEl: '.headerBlock .swiper-button-prev',
 		},
+		pagination: {
+			el: ".swiper-pagination",
+			type: 'bullets',
+		},
 	});
 
-
-	// список данных
-	// fetch('https://api.thedogapi.com/v1/breeds?limit=10&page=0')  
-	// .then(
-	// 	function(response) {
-	// 		if (response.status !== 200) {  
-	// 			console.log('0' +  
-	// 				response.status);
-	// 			return;
-	// 		}
-	// 		// Examine the text in the response  
-	// 		response.json().then(function(data) {  
-	// 			console.log(data);  
-	// 		});
-	// 	}
-	// )
-  // .catch(function(err) {  
-  //   console.log('Fetch Error :-S', err);  
-  // });
 }
 if (document.readyState !== 'loading') {
 	eventHandler();
